e18c8b58f6c41c47c2c7917ba778cfdaec4d52ab
daggerly <dagger003@163.com>
2017-09-16 23:27:27 +0800

    增加搜索框

--- danci/admin.py
+++ danci/admin.py
@@ -4,7 +4,7 @@ from models import Danci, WordmodeRecord, MeaningmodeRecord
 class DanciAdmin(admin.ModelAdmin):
     list_display = ('word', 'meaning', 'created_dt')
     ordering = ['-created_dt']
-
+    search_fields = ['word']
 class RecordAdmin(admin.ModelAdmin):
     list_display = ('danci', 'corrected', 'last_dt', 'next_dt', 'created_dt')
     ordering = ['-last_dt', '-created_dt']
